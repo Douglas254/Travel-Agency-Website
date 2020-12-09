15147881a419d4952f45cce267478c9809eb589a
Douglas254 <douglasobara97@gmail.com>
2020-12-09 19:05:29 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link }from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -7,7 +8,9 @@ const Footer = () => {
                 <li><button>Home</button></li>
                 <li><button>Privacy policy</button></li>
             </ul>
-
+            <div>
+                <h4>Designed and Developed by<span><Link> Douglas254</Link></span></h4>
+            </div>
             <ul>
                 <li><button>Book your Flight</button></li>
                 <li><button>Give away</button></li>
